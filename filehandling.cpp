aac2c1488197ac8edca7837f8dd4df4826d25997
Anujchobe <98049418+Anujchobe@users.noreply.github.com>
2023-03-03 11:27:16 +0530

    File handling

---
+++ filehandling.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    char data[100];
+    ofstream outfile;
+    outfile.open("test.txt");
+    cout<<"\n Writing in file";
+    cout<<"\n Enter name";
+    cin.getline(data,10);
+    outfile<<data<<endl;
+    cout<<"Enter ph no.";
+    cin.getline(data,10);
+    outfile<<data<<endl;
+    return 0;
+}
