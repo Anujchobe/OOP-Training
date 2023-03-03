7d2e596108dcd888bdb95e665591ba06c0ba7401
Anujchobe <98049418+Anujchobe@users.noreply.github.com>
2023-03-03 11:55:20 +0530

    Append

---
+++ fileappend.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    char data[100];
+    ofstream outfile;
+    outfile.open("test.txt",ios::app);
+    cout<<"\n Writing in file";
+    cout<<"\n Enter name";
+    cin.getline(data,10);
+    outfile<<data<<endl;
+    cout<<"Enter ph no.";
+    cin.getline(data,10);
+    outfile<<data<<endl;
+    return 0;
+}
